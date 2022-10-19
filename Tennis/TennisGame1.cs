33eb29b529ca2d1283011585f9bdaddcf13bfae6
Dave Bage <david_bage@hotmail.com>
2022-10-19 17:26:41 +0100

    Refactor matching scores into dictionary and lookup function

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Tennis
 {
     public class TennisGame1 : ITennisGame
@@ -5,6 +7,7 @@ namespace Tennis
         private int _player1Score = 0;
         private int _player2Score = 0;
         private readonly string[] _score = new string[] { "Love", "Fifteen", "Thirty", "Forty" };
+        private readonly Dictionary<int, string> _matchingScores = new Dictionary<int, string>() { {0, "Love-All"}, {1, "Fifteen-All"}, {2, "Thirty-All"} };
 
         public TennisGame1(string player1Name, string player2Name)
         {
@@ -24,36 +27,41 @@ namespace Tennis
             var tempScore = 0;
             if (_player1Score == _player2Score)
             {
-                switch (_player1Score)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                return GetMatchingScore();
             }
             else if (_player1Score >= 4 || _player2Score >= 4)
             {
                 var minusResult = _player1Score - _player2Score;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                if (minusResult == 1)
+                {
+                    score = "Advantage player1";
+                }
+                else if (minusResult == -1)
+                {
+                    score = "Advantage player2";
+                }
+                else if (minusResult >= 2)
+                {
+                    score = "Win for player1";
+                }
+                else
+                {
+                    score = "Win for player2";
+                }
             }
             else
             {
                 return $"{GetScore(_player1Score)}-{GetScore(_player2Score)}";
             }
             return score;
+        }
+
+        private string GetMatchingScore()
+        {
+            if(_matchingScores.ContainsKey(_player1Score))
+                return _matchingScores[_player1Score];
+
+            return "Deuce";
         }
 
         private string GetScore(int tempScore)
